2d4e85d5b7982d4edddc65d38e2d8c30ad8f490a
Jon Arild Karlsen <karlsen.jonarild@gmail.com>
2018-04-27 23:06:16 +0200

    FEAT: book dropdown menu + styling

--- src/Book.js
+++ src/Book.js
@@ -1,13 +1,63 @@
+/* Dropdown menu functionality from
+ * https://blog.campvanilla.com/reactjs-dropdown-menus-b6e06ae3a8fe
+ */
+
 import React, { Component } from 'react';
 import './book.css';
 
 class Book extends Component {
+    state = {
+        showMenu: false
+    };
+
+    showMenu(event) {
+        event.preventDefault();
+
+        this.setState({ showMenu: true }, () => {
+            document.addEventListener('click', this.closeMenu);
+        })
+    }
+
+    closeMenu(event) {
+        if (!this.dropdownMenu.contains(event.target)) {
+            this.setState({ showMenu: false }, () => {
+                document.removeEventListener('click', this.closeMenu);
+            });
+        }
+    }
+    
+    showMenu = this.showMenu.bind(this);
+    closeMenu = this.closeMenu.bind(this);
+
     render() {
         return (
             <article className="book">
                 <figure>
                     <img src={this.props.coverURL} alt="book cover" />
-                    <button className="book-control">+</button>
+                    <div className="book-dropbtn-container">
+                        <button 
+                            className="book-dropbtn"
+                            onClick={this.showMenu}
+                        >+</button>
+                        {this.state.showMenu ? ( 
+                            <div 
+                                className="menu"
+                                ref={(element) => {
+                                    this.dropdownMenu = element;
+                                }}
+                            >
+                                <button>Move to:</button>
+                                <button>Currently Reading</button>
+                                <button>Want to Read</button>
+                                <button>Have Read</button>
+                                <button>None</button>
+                            </div> 
+                            ) 
+                            : (
+                                null
+                            )
+                        }
+                    </div>
                     <figcaption>
                         <p className="book-title">
                             {this.props.title}
